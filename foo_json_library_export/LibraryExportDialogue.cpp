8bc5b17e398933a1f370dbc0b62a94294fc344a6
Ben Hymers <b.hymers@gmail.com>
2014-04-30 15:04:06 +0100

    Save all available data from tracks (metadata, info etc).
    Save metadata (closes #1)
    Save technical info (closes #2)
    Save ReplayGain data (closes #3)
    Save subsong_index and track length (closes #4)

--- foo_json_library_export/LibraryExportDialogue.cpp
+++ foo_json_library_export/LibraryExportDialogue.cpp
@@ -88,18 +88,107 @@ private:
 			rapidjson::Value trackValue;
 			trackValue.SetObject();
 
-			// todo: iterate fileInfo adding to trackValue.
+			// Track properties.
 
-			// String value for file path. No need to copy string (by passing rapidjson allocator)
+			// No need to copy string (by passing rapidjson allocator)
 			// as foobar guarantees string is valid until metadb handle is released,
 			// which will be after we've saved the file.
+			// In general though, most strings below will need to be copied as the API doesn't guarantee they'll stick around.
 			rapidjson::Value pathValue(track->get_path());
 			trackValue.AddMember("path", pathValue, allocator);
 
-			rapidjson::Value fileSizeValue(track->get_filesize());
-			trackValue.AddMember("fileSize", fileSizeValue, allocator);
+			rapidjson::Value subsongIndexValue(track->get_subsong_index());
+			trackValue.AddMember("subsong_index", subsongIndexValue, allocator);
 
-			// Finally, add it to the document.
+			// Let's not bother saving out timestamp and file size;
+			// these are properties of the files themselves which require no special parsing or decoding.
+
+			// This is the only thing the file_info struct has that isn't calculated from other fields.
+			// e.g. "number of samples" which the foobar interface shows in a track's properties,
+			// is actually calculated from length and bitrate.
+			rapidjson::Value lengthValue(fileInfo->get_length());
+			trackValue.AddMember("length", lengthValue, allocator);
+
+			// Replaygain data.
+			const auto& replay_gain_info = fileInfo->get_replaygain();
+			const bool is_album_gain_present = replay_gain_info.is_album_gain_present();
+			const bool is_album_peak_present = replay_gain_info.is_album_peak_present();
+			const bool is_track_gain_present = replay_gain_info.is_track_gain_present();
+			const bool is_track_peak_present = replay_gain_info.is_track_peak_present();
+
+			if(is_album_gain_present || is_album_peak_present || is_track_gain_present || is_track_peak_present)
+			{
+				rapidjson::Value replayGainContainerValue;
+				replayGainContainerValue.SetObject();
+
+				if(replay_gain_info.is_album_gain_present())
+				{
+					rapidjson::Value replayGainValue(replay_gain_info.m_album_gain);
+					replayGainContainerValue.AddMember("album_gain", replayGainValue, allocator);
+				}
+
+				if(replay_gain_info.is_album_peak_present())
+				{
+					rapidjson::Value replayGainValue(replay_gain_info.m_album_peak);
+					replayGainContainerValue.AddMember("album_peak", replayGainValue, allocator);
+				}
+
+				if(replay_gain_info.is_track_gain_present())
+				{
+					rapidjson::Value replayGainValue(replay_gain_info.m_track_gain);
+					replayGainContainerValue.AddMember("track_gain", replayGainValue, allocator);
+				}
+
+				if(replay_gain_info.is_track_peak_present())
+				{
+					rapidjson::Value replayGainValue(replay_gain_info.m_track_peak);
+					replayGainContainerValue.AddMember("track_peak", replayGainValue, allocator);
+				}
+
+				trackValue.AddMember("replaygain", replayGainContainerValue, allocator);
+			}
+
+			// 'info', which is technical details about the file.
+			if(fileInfo->info_get_count() > 0)
+			{
+				rapidjson::Value infoValue;
+				infoValue.SetObject();
+
+				for(t_size i = 0; i < fileInfo->info_get_count(); ++i)
+				{
+					rapidjson::Value individualInfoValue(fileInfo->info_enum_value(i), allocator);
+					infoValue.AddMember(fileInfo->info_enum_name(i), allocator, individualInfoValue, allocator);
+				}
+
+				trackValue.AddMember("info", infoValue, allocator);
+			}
+
+			// 'meta', which are metadata about the track, normally called its 'tags'.
+			// Note that unlike 'info', all meta fields can be multi-valued, so we save them out as arrays.
+			// todo: add option to save single-valued fields as values directly rather than one-element arrays.
+			if(fileInfo->meta_get_count() > 0)
+			{
+				rapidjson::Value metaValue;
+				metaValue.SetObject();
+
+				for(t_size i = 0; i < fileInfo->meta_get_count(); ++i)
+				{
+					rapidjson::Value individualMetaValue;
+					individualMetaValue.SetArray();
+					individualMetaValue.Reserve(fileInfo->meta_enum_value_count(i), allocator);
+
+					for(t_size j = 0; j < fileInfo->meta_enum_value_count(i); ++j)
+					{
+						individualMetaValue.PushBack(fileInfo->meta_enum_value(i, j), allocator);
+					}
+
+					metaValue.AddMember(fileInfo->meta_enum_name(i), allocator, individualMetaValue, allocator);
+				}
+
+				trackValue.AddMember("meta", metaValue, allocator);
+			}
+
+			// Finally, add the whole track object to the document.
 			document.PushBack(trackValue, allocator);
 		});
 
