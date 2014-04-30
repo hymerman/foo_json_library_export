aae3e9911a7c7ac3ece723caf8e5d9915eab37ba
Ben Hymers <b.hymers@gmail.com>
2014-04-30 13:42:06 +0100

    Fill in component "about" message

--- foo_json_library_export/Component.h
+++ foo_json_library_export/Component.h
@@ -4,7 +4,7 @@
 #define COMPONENT_FILENAME "foo_json_library_export.dll"
 #define COMPONENT_VERSION "1.0.0"
 #define COMPONENT_ABOUT_MESSAGE\
-	"TODO.\n\n"\
-	"TODO,\n"\
-	"TODO\n"\
-	"TODO."
+	COMPONENT_NAME " version " COMPONENT_VERSION "\n\n"\
+	"Exports the contents of the music library as a JSON file.\n\n"\
+	"For more information see:\n"\
+	"https://github.com/hymerman/foo_json_library_export\n"
